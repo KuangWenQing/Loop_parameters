cb03401d208c517c0533b5e2339dbb6a5d057851
jqiu <you@example.com>
2020-11-18 19:15:09 +0800

    2020/11/18 back-up

--- Loop_Parameter_convert.py
+++ Loop_Parameter_convert.py
@@ -1,12 +1,14 @@
 import numpy as np
 import math
 import re
+from builtins import print
 gps_fs =2*1024000.0
 gps_fs1 =8*1024000.0
 gps_car_norm = (2**31)/gps_fs
 gps_car_norm1 =(2**31)/gps_fs1 #
 # gps_code_norm = gps_car_norm1*2.0
 # gps_car_norm1_7 = gps_car_norm1/(2.0**7)
+
 class control:
     high = 0
     mid = 1
@@ -14,6 +16,17 @@ class control:
     num =3
 
 class para_convert:
+    gl_nCn0 = 'U32 nCn0[10] = {'
+    gl_nSmooth = 'U32 nSmooth[2][2] = '
+    gl_nThreshold = 'U32 nThreshold[3][2] = {'
+    gl_nPll2 = 'U32 nPll2[3][5][6] = {{'
+    gl_nLongPll = "U32 nLongPll[5][6] = {"
+    gl_nPll3 = "U32 nPll3[3][5][9] = {{"
+    gl_nFll2 = "U32 nFll2[3][4][3] = {{"
+    gl_nFll3 = "U32 nFll3[3][4][6] = {{"
+    gl_nDllBit = "U32 nDllBit[3][6] = {"
+    gl_nDllSum1 = "U32 nDllSum1[3][2][3] = {{"
+    gl_nDllSum2 = "U32 nDllSum2[6][3] = {"
     def __init__(self):
         self.gps_code_norm = gps_car_norm1 * 2.0
         self.gps_car_norm1_7 = gps_car_norm1 / (2.0 ** 7)
@@ -476,7 +489,131 @@ class para_convert:
         #     temp_coe = [pll3_coe_l, pll3_coe_h, pll3_frac, pll3_coe_l2, pll3_coe_h2, pll3_frac2, pll3_coe_l3, pll3_coe_h3, pll3_frac3]
         #     coe.append(temp_coe)
         return coe
-
+    def hexPrint(self, arr, head):
+        if head == "PLL ??ms:":
+            para_convert.gl_nLongPll = "U32 nLongPll[5][6] = {"
+        if head == "DLL 20ms:":
+            para_convert.gl_nDllSum2 = "U32 nDllSum2[6][3] = {"
+        idx = 0
+        for each_arr in arr:
+            print(head, end=' ')
+            if head == "CN0:":
+                if len(para_convert.gl_nCn0) < 33:
+                    for item in each_arr:
+                        if item < 0:
+                            item = 2 ** 32 + item
+                        para_convert.gl_nCn0 += '0x{:0>8X}, '.format(item)
+                    para_convert.gl_nCn0 = para_convert.gl_nCn0[:-2] + "};"
+            if head == "PLL ??ms:":
+                if idx < 5:
+                    para_convert.gl_nPll2 += '{'
+                    for item in each_arr:
+                        if item < 0:
+                            item = 2**32+item
+                            para_convert.gl_nPll2 += '0x{:X}, '.format(item)
+                        elif item > 65535:
+                            para_convert.gl_nPll2 += '0x{:0>8X}, '.format(item)
+                        else:
+                            para_convert.gl_nPll2 += '0x{:0>4X}, '.format(item)
+                    para_convert.gl_nPll2 = para_convert.gl_nPll2[:-2] + '},\n\t\t\t\t\t\t'
+                if idx > 4:
+                    para_convert.gl_nLongPll += "{"
+                    for item in each_arr:
+                        if item < 0:
+                            item = 2**32+item
+                            para_convert.gl_nLongPll += '0x{:X}, '.format(item)
+                        elif item > 65535:
+                            para_convert.gl_nLongPll += '0x{:0>8X}, '.format(item)
+                        else:
+                            para_convert.gl_nLongPll += '0x{:0>4X}, '.format(item)
+                    para_convert.gl_nLongPll = para_convert.gl_nLongPll[:-2] + '},\n\t\t\t\t\t\t'
+            if head == "PLL3 ?ms:":
+                para_convert.gl_nPll3 += '{'
+                for item in each_arr:
+                    if item < 0:
+                        item = 2 ** 32 + item
+                        para_convert.gl_nPll3 += '0x{:X}, '.format(item)
+                    elif item > 65535:
+                        para_convert.gl_nPll3 += '0x{:0>8X}, '.format(item)
+                    else:
+                        para_convert.gl_nPll3 += '0x{:0>4X}, '.format(item)
+                para_convert.gl_nPll3 = para_convert.gl_nPll3[:-2] + '},\n\t\t\t\t\t\t'
+            if head == "FLL ?ms:":
+                para_convert.gl_nFll2 += "{"
+                for item in each_arr:
+                    if item < 0:
+                        item = 2 ** 32 + item
+                        para_convert.gl_nFll2 += '0x{:X}, '.format(item)
+                    elif item > 65535:
+                        para_convert.gl_nFll2 += '0x{:0>8X}, '.format(item)
+                    else:
+                        para_convert.gl_nFll2 += '0x{:0>4X}, '.format(item)
+                para_convert.gl_nFll2 = para_convert.gl_nFll2[:-2] + '},\n\t\t\t\t\t\t'
+            if head == "FLL3 ?ms:":
+                para_convert.gl_nFll3 += '{'
+                for item in each_arr:
+                    if item < 0:
+                        item = 2 ** 32 + item
+                        para_convert.gl_nFll3 += '0x{:X}, '.format(item)
+                    elif item > 65535:
+                        para_convert.gl_nFll3 += '0x{:0>8X}, '.format(item)
+                    else:
+                        para_convert.gl_nFll3 += '0x{:0>4X}, '.format(item)
+                para_convert.gl_nFll3 = para_convert.gl_nFll3[:-2] + '},\n\t\t\t\t\t\t'
+            if head == "DLL 1ms:":
+                para_convert.gl_nDllBit += "{"
+                for item in each_arr:
+                    if item < 0:
+                        item = 2 ** 32 + item
+                        para_convert.gl_nDllBit += '0x{:X}, '.format(item)
+                    elif item > 65535:
+                        para_convert.gl_nDllBit += '0x{:0>8X}, '.format(item)
+                    else:
+                        para_convert.gl_nDllBit += '0x{:0>4X}, '.format(item)
+                para_convert.gl_nDllBit = para_convert.gl_nDllBit[:-2] + '},\n\t\t\t\t\t\t'
+            if head == "DLL 20ms:":
+                if idx < 2:
+                    para_convert.gl_nDllSum1 += '{'
+                    for item in each_arr:
+                        if item < 0:
+                            item = 2**32+item
+                            para_convert.gl_nDllSum1 += '0x{:X}, '.format(item)
+                        elif item > 65535:
+                            para_convert.gl_nDllSum1 += '0x{:0>8X}, '.format(item)
+                        else:
+                            para_convert.gl_nDllSum1 += '0x{:0>4X}, '.format(item)
+                    para_convert.gl_nDllSum1 = para_convert.gl_nDllSum1[:-2] + '},\n\t\t\t\t\t\t'
+                if idx > 1:
+                    para_convert.gl_nDllSum2 += "{"
+                    for item in each_arr:
+                        if item < 0:
+                            item = 2**32+item
+                            para_convert.gl_nDllSum2 += '0x{:X}, '.format(item)
+                        elif item > 65535:
+                            para_convert.gl_nDllSum2 += '0x{:0>8X}, '.format(item)
+                        else:
+                            para_convert.gl_nDllSum2 += '0x{:0>4X}, '.format(item)
+                    para_convert.gl_nDllSum2 = para_convert.gl_nDllSum2[:-2] + '},\n\t\t\t\t\t\t'
+            for item in each_arr:
+                if item < 0:
+                    item = 2**32+item
+                    print('0x{:X},'.format(item), end=' ')
+                elif item > 65535:
+                    print('0x{:0>8X},'.format(item), end=' ')
+                else:
+                    print('0x{:0>4X},'.format(item), end=' ')
+            print()
+            idx += 1
+        if head == "PLL ??ms:":
+            para_convert.gl_nPll2 = para_convert.gl_nPll2[:-8] + '},\n\t\t\t\t\t{\t'
+        if head == "PLL3 ?ms:":
+            para_convert.gl_nPll3 = para_convert.gl_nPll3[:-8] + '},\n\t\t\t\t\t{\t'
+        if head == "FLL ?ms:" and idx > 1:
+            para_convert.gl_nFll2 = para_convert.gl_nFll2[:-8] + '},\n\t\t\t\t\t{\t'
+        if head == "FLL3 ?ms:" and idx > 1:
+            para_convert.gl_nFll3 = para_convert.gl_nFll3[:-8] + '},\n\t\t\t\t\t{\t'
+        if head == "DLL 20ms:":
+            para_convert.gl_nDllSum1 = para_convert.gl_nDllSum1[:-8] + '},\n\t\t\t\t\t{\t'
     def cal(self,dll,pll,pll_long,pll3,fll,fll3,dll_bit,fll_bit,fll3_bit,all_bn='',mode=0,rank =control.high, cnf = [], gps_cn0_db=[], smooth_fac_factor=[]):
         if mode ==int(b'10',2):
             dll_bit = np.append(dll_bit,dll)
@@ -486,16 +623,36 @@ class para_convert:
             dll_bit_m = self.dll_bit(dll_bit,self.gps_code_norm)#todo
             fll_bit_m = self.fll_bit(fll_bit,self.gps_car_norm1_7)
             para = [pll_m, fll_bit_m, fll_m,dll_bit_m]
+            print('-*-*-', dll_bit, fll_m, pll_m, dll_bit_m, fll_bit_m, para, '-*-*-')
         elif mode==int(b'00',2):
             dll_m = self.dll_sum(dll, self.gps_code_norm)
             fll_m = self.fll_sum(fll, self.gps_car_norm1_7)
             fll3_m = self.fll_3_sum(fll3, self.gps_car_norm1_7)##todo
             pll_m = self.pll(pll, pll_long, all_bn,self.gps_car_norm1_7)##todo
             pll3_m = self.pll3(pll3, self.gps_car_norm1_7)##todo
+
             dll_bit_m = self.dll_bit(dll_bit,self.gps_code_norm)#todo
             fll_bit_m = self.fll_bit(fll_bit,self.gps_car_norm1_7)
             fll3_bit_m = self.fll_bit_3(fll3_bit,self.gps_car_norm1_7)
             para = [pll_m,pll3_m,fll_bit_m,fll_m,fll3_bit_m,fll3_m,dll_bit_m,dll_m]
+            #self.hexPrint([list(gps_cn0_db)], "CN0:")
+            #self.hexPrint([smooth_fac_factor], "PLL smooth:")
+            #self.hexPrint([], "FLL threshold:")
+            self.hexPrint(pll_m, "PLL ??ms:")
+            self.hexPrint(pll3_m, "PLL3 ?ms:")
+
+            self.hexPrint(fll_bit_m, "FLL ?ms:")
+            self.hexPrint(fll_m, "FLL ?ms:")
+            self.hexPrint(fll3_bit_m, "FLL3 ?ms:")
+            self.hexPrint(fll3_m, "FLL3 ?ms:")
+
+            self.hexPrint(dll_bit_m, "DLL 1ms:")
+            self.hexPrint(dll_m, "DLL 20ms:")
+            print("FLL threshold:", "0x{:0>2X}{:0>2X}{:0>2X}{:0>2X}".format(cnf[3], cnf[2], cnf[1], cnf[0]))
+            print("PLL threshold:", "0x{:0>2X}{:0>2X}{:0>2X}{:0>2X}".format(cnf[7], cnf[6], cnf[5], cnf[4]))
+            para_convert.gl_nThreshold += " {}0x{:0>2X}{:0>2X}{:0>2X}{:0>2X}, 0x{:0>2X}{:0>2X}{:0>2X}{:0>2X}{},"\
+                .format("{", cnf[3], cnf[2], cnf[1], cnf[0], cnf[7], cnf[6], cnf[5], cnf[4], "}")
+            # print('-*-*-', dll_m, fll_m, fll3_m, pll_m, pll3_m, dll_bit_m, fll_bit_m, fll3_bit_m, para, '-*-*-')
         new_para=list(flat(para))
         # print(new_para)
         result=self.hex_to_dec(new_para,rank, cnf, gps_cn0_db, smooth_fac_factor)
@@ -525,12 +682,20 @@ class para_convert:
         #                            np.ceil((1 - fli_smooth_factor) * (2.0 ** smooth_fac_shift1))], dtype=int)
         fli_smooth_par = np.floor(fli_smooth_factor * (2.0 ** smooth_fac_shift0) + 0.5).astype(int)
         f_parameter = [fli_smooth_par[0] & 0xff, fli_smooth_par[0] >> 8, fli_smooth_par[1] & 0xff, fli_smooth_par[1] >> 8]
+        print("FLL smooth:", "0x{:0>2X}{:0>2X},".format(f_parameter[1], f_parameter[0]), "0x{:0>2X}{:0>2X},".format(f_parameter[3], f_parameter[2]))
+
         pli_smooth_factor = smooth_fac_factor[3]
         # pli_smooth_par = np.array([np.ceil(pli_smooth_factor * (2.0 ** smooth_fac_shift0)),
         #                            np.ceil((1 - pli_smooth_factor) * (2.0 ** smooth_fac_shift1))], dtype=int)
         pli_smooth_par = np.floor(pli_smooth_factor * (2.0 ** smooth_fac_shift1) + 0.5).astype(int)
         p_parameter = [pli_smooth_par[0] & 0xff, pli_smooth_par[0] >> 8, pli_smooth_par[1] & 0xff, pli_smooth_par[1] >> 8]
-
+        print("PLL smooth:", "0x{:0>2X}{:0>2X},".format(p_parameter[1], p_parameter[0]),
+              "0x{:0>2X}{:0>2X},".format(p_parameter[3], p_parameter[2]))
+        if len(para_convert.gl_nSmooth) < 33:
+            para_convert.gl_nSmooth += "{}0x{:0>2X}{:0>2X}, 0x{:0>2X}{:0>2X}{},".format("{{",
+                                    p_parameter[1], p_parameter[0], p_parameter[3], p_parameter[2], "}")
+            para_convert.gl_nSmooth += "{}0x{:0>2X}{:0>2X}, 0x{:0>2X}{:0>2X}{};".format(" {", f_parameter[1], f_parameter[0],
+                                    f_parameter[3], f_parameter[2], "}}")
         send_data = [0xaa ,0xaa]
         lenth = len(data)
         bit_32 = 3*np.arange(1,100) -1##96/3  =32    3*11(50) 给足够长
@@ -539,6 +704,9 @@ class para_convert:
         # nwr_fl_clns = np.floor(32 * np.log2(((fl_cn0_nwr - 1.0)/(20.0 - fl_cn0_nwr))) * (2**16)).astype(int)
 
         gps_cn0_th = (np.floor((gps_cn0_db/10 - 3) * (2**(5 + smooth_fac_shift1))/np.log10(2) + 0.5)).astype(int)
+        self.hexPrint([list(gps_cn0_th)], "CN0:")
+
+
         # gps_cn0_th = nwr_fl_clns.tolist()
         i = 0
         for num in gps_cn0_th:
@@ -868,16 +1036,22 @@ if __name__ == '__main__':
     #                    [4.0, 3.5, 3.0], [2.0, 2.0, 2.0], [1.0, 0.6, 0.4]])  # 1ms, 2ms, 4ms, 10ms, 20ms, 100ms
     # all_bn = np.array([1.0,0.6,0.5,0.4,0.3])# 2ms, 4ms, 10ms, 20ms, 100ms
     A = para_convert()
-    pll_long_bn = np.array([1.5, 1.0, 0.8, 0.8, 0.7])
+    pll_group_4 = 2.0
+    pll_long_bn = np.array([1.2, 1.0, 1.0, 1.0, 0.4]) #1103
+    #pll_long_bn = np.array([1.5, 1.0, 0.8, 0.8, 0.7])
     smooth_fac_shift0 = 16
     smooth_fac_shift1 = 10
     fli_smooth_factor = np.array([0.1, 0.3])
     pli_smooth_factor = np.array([0.04, 0.02])
-    #gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.5, 20.0, 18.5, 17.0, 39.0, 35.0])
-    gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.5, 20.0, 18.5, 17.0, 00.0, 00.0]) # disable
-    # gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.5, 20.0, 18.5, 17.0, 39.0, 35.0])  # enable
+    #gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.5, 20.0, 18.5, 17.0, 39.0, 39.0])
+    # gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.5, 20.0, 18.5, 17.0, 39.0, 00.0]) # disable
+    # gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.0, 19.5, 18.0, 16.5, 39.0, 39.0])  # enable 1103
+    gps_cn0_db = np.array([33.0, 29.8, 27.3, 21, 0, 0, 0, 0, 39.0, 39.0])  # enable 1103
+    # gps_cn0_db = np.array([33.0, 28.5, 25, 20, 0, 0, 0, 0, 39.0, 39.0])  # enable 1103
+    #gps_cn0_db = np.array([33.0, 29.8, 27.3, 25.0, 22.5, 20.0, 18.5, 17.0, 39.0, 39.0])  # enable
     # gps_cn0_db = np.array([35.0, 31.0, 28.3, 26.0, 23.5, 21.0, 19.0, 17.0, 39.0, 35.0])
     narrow_dll_bn = 1.0
+    narrow_long_dll_bn =[narrow_dll_bn, 0.2, 0.1, 0.1, 0.05, 0.05]
 
     smooth_fac_factor = list()
     smooth_fac_factor.append(16)
@@ -887,12 +1061,12 @@ if __name__ == '__main__':
 
     ###high
     dll_bn = np.array([2.0])
-    dll_sum_bn = np.array([2.0, 1.0,  # 1.0 # 0.5~1.0
-                           narrow_dll_bn, 0.05, 0.03, 0.02, 0.02, 0.01])  # narrow, 2*20, 5*20, 10*20, 15*20, 30*20
+    dll_sum_bn = np.array([2.0, 1.0] + narrow_long_dll_bn)  # narrow, 2*20, 5*20, 10*20, 15*20, 30*20
 
     fll_bn = np.array([1.5])
-    fll_sum_bn = np.array([[20], [16], [12.0]])
-    pll_bn = np.array([[16], [14], [10], [6.0], [2.0]])
+    fll_sum_bn = np.array([[30], [20], [8]])
+    pll_bn = np.array([[12], [8], [6], [3.0], [pll_group_4]]) #1103
+    #pll_bn = np.array([[16], [14], [10], [6.0], [2.0]])
     fll3_bn = np.array([3.0])
     fll3_sum_bn = np.array([[20], [16], [12.0]])
     pll3_bn = np.array([[28], [22], [18], [12], [6.0]])
@@ -907,11 +1081,12 @@ if __name__ == '__main__':
     mode = 0
     rank = control.mid
     dll_bn = np.array([1.0])
-    dll_sum_bn = np.array([1.2, 0.5,  # 0.6, 0.3, 0.15, 0.1
-                           narrow_dll_bn, 0.05, 0.03, 0.02, 0.02, 0.01])
+    dll_sum_bn = np.array([1.2, 0.6]  # 0.6, 0.3, 0.15, 0.1
+                           + narrow_long_dll_bn)
     fll_bn = np.array([1.5])
-    fll_sum_bn = np.array([[10], [6], [3.5]])
-    pll_bn = np.array([[10], [8], [6], [4.0], [1.5]])
+    fll_sum_bn = np.array([[20], [12], [5]])
+    # pll_bn = np.array([[10], [8], [6], [4.0], [1.5]])
+    pll_bn = np.array([[10], [8], [6.0], [3.0], [pll_group_4]])
     fll3_bn = np.array([1.5])
     fll3_sum_bn = np.array([[16], [12], [6.0]])
     pll3_bn = np.array([[12], [8], [6], [5], [3]])
@@ -925,17 +1100,24 @@ if __name__ == '__main__':
     mode = 0
     rank = control.low
     dll_bn = np.array([0.4])
-    dll_sum_bn = np.array([1.0, 0.3,
-                           narrow_dll_bn, 0.05, 0.03, 0.02, 0.02, 0.01])
+    # dll_sum_bn = np.array([1.0, 0.2,
+    #                        narrow_dll_bn, 0.05, 0.03, 0.02, 0.02, 0.01])
+    dll_sum_bn = np.array([1.0, 0.1]
+                           + narrow_long_dll_bn)
     fll_bn = np.array([0.6])
-    fll_sum_bn = np.array([[8], [4], [2.0]])
-    pll_bn = np.array([[8], [6], [4], [3.0], [1.5]])
+    '''old par'''
+    # pll_bn = np.array([[8], [6], [4], [3.0], [1.5]])
+    # pll_bn = np.array([[8], [6], [4], [3.0], [pll_group_4]])
+    '''new par'''
+    fll_sum_bn = np.array([[60], [8.75], [2.0]])
+    pll_bn = np.array([[12], [8], [4], [3.0], [pll_group_4]])
+
     fll3_bn = np.array([1.0])
     fll3_sum_bn = np.array([[12], [8], [4.0]])
-    pll3_bn = np.array([[10], [4], [4], [3], [2.0]])
+    pll3_bn = np.array([[10], [8], [6.0], [3.5], [1.0]])
     all_bn = []
     # limit_cfg = [12, 100, 50, 16, 35, 22, 17, 12]
-    limit_cfg = [12, 100, 50, 16, 35, 32, 23, 17]
+    limit_cfg = [12, 100, 50, 16, 36, 32, 23, 17]
     print("the low state:")
     result = A.cal(dll_sum_bn, pll_bn, pll_long_bn, pll3_bn, fll_sum_bn,fll3_sum_bn, dll_bn, fll_bn, fll3_bn, all_bn, mode, rank, limit_cfg, gps_cn0_db, smooth_fac_factor)
 
@@ -943,7 +1125,45 @@ if __name__ == '__main__':
     #  "pll_3": 7.377932579656648, "pll_2": 9.231968331200513, "fll": 13.687896404735579, "pll_1": 11.848610966939232,
     #  "fll_1": 7.021750488014408, "dll": 2.1373946981326593, "pll": 35.68908386923844, "dll_bit": 0.7025611528814057}
 
-    print(result)
+    print(A.gl_nCn0)
+    print(A.gl_nSmooth)
+    para_convert.gl_nThreshold = para_convert.gl_nThreshold[:-1] + "};"
+    print(A.gl_nThreshold)
+    para_convert.gl_nPll2 = para_convert.gl_nPll2[:-9] + '};'
+    print(para_convert.gl_nPll2)
+    para_convert.gl_nLongPll = para_convert.gl_nLongPll[:-8] + '};'
+    print(para_convert.gl_nLongPll)
+    para_convert.gl_nPll3 = para_convert.gl_nPll3[:-9] + '};'
+    print(para_convert.gl_nPll3)
+    para_convert.gl_nFll2 = para_convert.gl_nFll2[:-9] + '};'
+    print(para_convert.gl_nFll2)
+    para_convert.gl_nFll3 = para_convert.gl_nFll3[:-9] + '};'
+    print(para_convert.gl_nFll3)
+    para_convert.gl_nDllBit = para_convert.gl_nDllBit[:-8] + '};'
+    print(para_convert.gl_nDllBit)
+    para_convert.gl_nDllSum1 = para_convert.gl_nDllSum1[:-9] + '};'
+    print(para_convert.gl_nDllSum1)
+    para_convert.gl_nDllSum2 = para_convert.gl_nDllSum2[:-8] + '};'
+    print(para_convert.gl_nDllSum2)
 
+    fd = open("tmp.c", 'w')
+    fd.write(A.gl_nCn0 + '\n' + A.gl_nSmooth + '\n' + A.gl_nThreshold + '\n'
+             + A.gl_nPll2 + '\n' + A.gl_nLongPll + '\n' + A.gl_nPll3 + '\n'
+             + A.gl_nFll2 + '\n' + A.gl_nFll3 + '\n' + A.gl_nDllBit + '\n'
+             + A.gl_nDllSum1 + '\n' + A.gl_nDllSum2)
+    fd.close()
+
+    a = np.array([[1,2 ,10],[4,5,11]])
+    def cvt_mul_intarr2hexarr(a):
+        shape_a = np.shape(a)
+        size_a = 1
+        for dim in shape_a:
+            size_a *= dim
+        a_flaten = a.reshape(size_a)
+        a_flaten_hex = [hex(item) for item in a_flaten]
+        a_hex = np.asarray(a_flaten_hex)
+        a_hex_renew = a_hex.reshape(shape_a)
+        a_hex_renew.tolist()
+        return a_hex_renew
 
 
